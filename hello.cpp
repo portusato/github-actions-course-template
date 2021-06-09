846ab922132151ff24f805e95b779fbecf07bb1c
Nicolas Morales <nicolas.morales@nuance.com>
2021-06-09 13:35:38 -0400

    Introduce vuln EXP50-CPP

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+void print_and_increment(int i);
+
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!\n";
+    print_and_increment(5);
     return 0;
+}
+
+void print_and_increment(int i) {
+    std::cout << i++ << i << std::endl;
 }
