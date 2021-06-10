dd9c4342997a1b048418746d99a9e798237e7c1f
portusato <portu.github@gmail.com>
2021-06-10 11:29:03 +0200

    Introduce stack mashing vulnerability to see if it's detected by codeQL

--- hello.cpp
+++ hello.cpp
@@ -3,11 +3,19 @@
 void print_and_increment(int i);
 
 int main() {
+    IsPasswordOK();
     std::cout << "Hello World!\n";
     print_and_increment(5);
+    
     return 0;
 }
 
 void print_and_increment(int i) {
     std::cout << i++ << i << std::endl;
+}
+
+bool IsPasswordOK(void) {
+    char password[12];
+    gets(password);
+    return 0 == strcmp(password, "goodpass");
 }
