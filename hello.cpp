fbe9d4ccff788797f152f5a9a5ada46024e090df
Nicolas Morales <nicolas.morales@nuance.com>
2021-06-10 06:37:29 -0400

    try to trigger rule of 3 error

--- hello.cpp
+++ hello.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
-#include <cstdio>
-#include <string.h>
-
-#include "stdio.h"
-using namespace std;
-
-void print_and_increment(int i);
-bool IsPasswordOK(void);
-
-int main() {
-    IsPasswordOK();
-    std::cout << "Hello World!\n";
-    print_and_increment(5);
-    
-    return 0;
-}
-
-void print_and_increment(int i) {
-    std::cout << i++ << i << std::endl;
-}
-
-bool IsPasswordOK(void) {
-    char buffer[12];
-    char offset[6];
-    std::cout << "Enter offset: ";
-    fgets(offset, 6, stdin);
-    std::cout << buffer [int(offset)];
-}
+#include <cstddef>
+#include <cstring>
+ 
+class rule_of_three
+{
+    char* cstring; // raw pointer used as a handle to a dynamically-allocated memory block
+    rule_of_three(const char* s, std::size_t n) // to avoid counting twice
+    : cstring(new char[n]) // allocate
+    {
+        std::memcpy(cstring, s, n); // populate
+    }
+ public:
+    rule_of_three(const char* s = "")
+    : rule_of_three(s, std::strlen(s) + 1)
+    {}
+    ~rule_of_three() // I. destructor
+    {
+        delete[] cstring;  // deallocate
+    }
+    rule_of_three(const rule_of_three& other) // II. copy constructor
+    : rule_of_three(other.cstring)
+    {}
+    /*rule_of_three& operator=(const rule_of_three& other) // III. copy assignment
+    {
+        if (this == &other) return *this;
+        std::size_t n{std::strlen(other.cstring) + 1};
+        char* new_cstring = new char[n]; // allocate
+        std::memcpy(new_cstring, other.cstring, n); // populate
+        delete[] cstring;  // deallocate
+        cstring = new_cstring;
+        return *this;
+    }*/
+ public:
+    operator const char *() const { return cstring; } // accessor
+};
+ 
+int main()
+{
+    rule_of_three o1{"abc"};
+    std::cout << o1 << ' ';
+    auto o2{ o1 }; // I. uses copy constructor
+    std::cout << o2 << ' ';
+    rule_of_three o3("def");
+    std::cout << o3 << ' ';
+    o3 = o2; // III. uses copy assignment
+    std::cout << o3 << ' ';
+}   // <- II. all destructors are called 'here'
