482de258cd0dd1d9e4b3a0a626e6e78948854290
Nicolas Morales <nicolas.morales@nuance.com>
2021-06-11 10:42:11 -0400

    Injected vulnerability (tempfile.mktemp) in hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("hello world\n")
+import tempfile
+file_name = tempfile.mktemp()
+print(file_name)
+
+with open(file_name, 'w') as fp:
+    fp.write("hello world\n")
+
+with open (file_name) as fp:
+    buf = fp.read()
+    print('Read from file:' + buf)
